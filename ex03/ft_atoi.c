ecf0957871883c00f9c184f28066d48c7639b9b4
kazukitkh <kazuki.tkh@gmail.com>
2021-08-13 10:08:45 +0900

    first commit

---
+++ ex03/ft_atoi.c
@@ -0,0 +1,48 @@
+#include <stdio.h>/*strが7桁になった瞬間にバグ起きる*/
+
+int		ft_atoi(char *str)
+{
+	int		nb[8];
+	
+	nb[0] = 0;/*the address of str*/
+	nb[1] = 0;
+	nb[4] = 1;
+	while (str[nb[0]] == 32 || (str[nb[0]] >= 9 && str[nb[0]] <= 13))
+		nb[0]++;
+	while (str[nb[0]] == 43 || str[nb[0]] == 45)
+	{
+		if (str[nb[0]] == 45)
+			nb[1] += 1;
+		nb[0]++;
+	}
+	while (nb[1])
+	{
+		nb[4] *= -1;
+		nb[1]--;
+	}/*fine until here*/
+	nb[2] = nb[0];
+	while (str[nb[0]] >= 48 && str[nb[0]] <= 57)
+		nb[0]++;
+	nb[3] = nb[0] - nb[2] - 1;
+	while (nb[1] <= nb[3])
+	{
+		nb[7] = nb[3] - nb[1];
+		nb[5] = str[nb[2]] - 48;
+		while(nb[7])
+		{
+			nb[5] *= 10;
+			nb[7]--;
+		}
+		nb[6] += nb[5];
+		nb[1]++;
+		nb[2]++;
+	}
+	nb[6] *= nb[4];
+	return (nb[6]);
+}
+
+int	main(void)
+{
+	char	str[] = "   89";
+	printf("%d", ft_atoi(str));
+}
